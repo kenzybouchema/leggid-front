230e3ba20b87939139ddf5000ed28ce4295b2f62
Kenzy B <kenzy.bouchema@gmail.com>
2021-01-10 00:10:28 +0100

    Leggid-front: Injection d'un client HTTP dans le service d'authentification, implémentationde la méthode sign-up

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { SignupRequestPayload } from '../sign-up/sign-up-request.payload';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  // Injection du client HTTP
+  constructor(private http: HttpClient) { }
+
+  signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
+    // On consomme l'API REST avec un POST depuis le client HTTP avec la 'signupRequestPayload'
+    return this.http.post('http://localhost:8080/api/auth/signup', signupRequestPayload);
+  }
 }
