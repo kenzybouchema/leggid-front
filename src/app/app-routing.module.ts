2ce86768acfda96996e57e2d245b1a426c67841f
Kenzy B <kenzy.bouchema@gmail.com>
2021-01-09 17:09:26 +0100

    Leggid-front: Ajout du composant sign-up, routage 2/2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+
+const routes: Routes = [
+  { path: 'sign-up', component: SignUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
     SignUpComponent
   ],
   imports: [
